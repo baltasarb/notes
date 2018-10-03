3ad186a4e70462ce0fa989a412c8eff00afa927f
baltasarb <baltasar.brito@gmail.com>
2018-10-03 22:29:17 +0100

    Update: first (hopefully) working version of event bus. lacks test.

--- concurrent-programming/jvm/src/main/java/EventBus.java
+++ concurrent-programming/jvm/src/main/java/EventBus.java
@@ -1,6 +1,8 @@
 import sun.plugin.dom.exception.InvalidStateException;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Consumer;
@@ -9,44 +11,92 @@ public class EventBus {
 
     private final int MAX_PENDING;
     private final ReentrantLock monitor;
-    private LinkedList<Subscriber> subscribers;
-    private boolean shutdown;
+    private Condition shutdown;
+    private HashMap<Class, LinkedList<Subscriber>> subscribers;
+    private boolean isShuttingDown;
+    private int numberOfSubscribers;
 
     public EventBus(int maxPending) {
         MAX_PENDING = maxPending;
         monitor = new ReentrantLock();
-        subscribers = new LinkedList<>();
-        shutdown = false;
+        subscribers = new HashMap<>();
+        isShuttingDown = false;
+        shutdown = monitor.newCondition();
+        numberOfSubscribers = 0;
     }
 
-    public <T> void SubscribeEvent(Consumer<T> handler) throws InterruptedException {
+    public <T> void SubscribeEvent(Consumer<T> handler, Class consumerType) throws InterruptedException {
         monitor.lock();
 
-        Subscriber subscriber = new Subscriber(monitor.newCondition(), handler.getClass());
-        subscribers.add(subscriber);
+        // TODO: should this be done ?? prevents new subscribers to be added on a system shutdown
+        if (isShuttingDown) {
+            monitor.unlock();
+            throw new InvalidStateException("The bus is shutting down and does not allow new subscriptions.");
+        }
 
-        try{
+        //informs the bus that a new subscriber has arrived
+        //usefull for correct shutdown, shutdown waits for this to reach 0
+        numberOfSubscribers++;
 
-            while(true){
-                subscriber.condition.await();
-                //even if spurious awakening, should the list be checked for handling?
-                if(subscriber.messages.size() > 0){
-                    //How to know type of handler is same as of message?
-                    for (int i = 0; i < subscriber.messages.size(); i++)
-                    subscriber.messages.forEach(message -> {
-                        try{
-                            handler.accept((T)message); //different types how to solve?
-                        }catch (ClassCastException e){
-                            //if its not the same type continue interating
-                        }
-                    });
+        Subscriber subscriber = new Subscriber(monitor.newCondition());
+
+        //if the type does not have a list, create it, then add the subscriber to it
+        if (subscribers.get(consumerType) == null) {
+            LinkedList<Subscriber> subscribersByType = new LinkedList<>();
+            subscribers.put(consumerType, subscribersByType);
+        }
+        subscribers.get(consumerType).add(subscriber);
+
+        try {
+            while (true) {
+
+                //  if publishing has occurred while handling last batch then it should be handled before waiting again
+                //  it also helps with message dumping on shutdown
+                if (subscriber.messages.isEmpty())
+                    //  wait for events to be published
+                    subscriber.condition.await();
+
+                //  on notification, check if there are messages to process and handle them if there are
+                if (!subscriber.messages.isEmpty()) {
+
+                    //  grab all messages and empty existing list, avoids further publishing to this list
+                    LinkedList<Object> messages = subscriber.messages;
+                    subscriber.messages = new LinkedList<>();
+
+                    //  sets flag to true so that the publisher knows if notification is needed to awake the subscriber
+                    subscriber.isHandlingMessages = true;
+
+                    //  handling outside the lock to avoid deadlocks: handler work is unknown
+                    monitor.unlock();
+
+                    handleMessages(messages, handler);
+                }
+
+                monitor.lock();
+
+                // tell the publisher that notification will be required if further publication is made
+                // one exception: if the there is publication while the subscriber is handling messages this will be false and set
+                // to true on the next iteration of the loop directly, redundant but necessary.
+                // this redundancy avoids a simple if to check list size > 0 here
+                subscriber.isHandlingMessages = false;
+
+                // if a shutdown is occurring guarantee that no more messages are left to publish before exit
+                if (isShuttingDown && subscriber.messages.isEmpty()) {
+                    numberOfSubscribers--;
+                    subscribers.get(consumerType).remove(subscriber);
+
+                    //last subscriber to exit, ready to shutdown completely, notify the shutdown agent
+                    if(numberOfSubscribers == 0)
+                        shutdown.signal();
+
+                    monitor.unlock();
+                    return;
                 }
             }
-
-        }catch (InterruptedException e){
-            subscribers.remove(subscriber);
+        } catch (InterruptedException e) {
+            subscribers.get(consumerType).remove(subscriber);
             throw e;
-        }finally {
+        } finally {
             monitor.unlock();
         }
     }
@@ -55,51 +105,85 @@ public class EventBus {
     public <E> void PublishEvent(E message) {
         monitor.lock();
 
-        if (shutdown) {
+        if (isShuttingDown) {
             monitor.unlock();
-            throw new InvalidStateException("The subscriber is shutting down and unavailable for further publishing.");
+            throw new InvalidStateException("The subscribers are shutting down and are unavailable for further publishing.");
         }
 
-        subscribers.forEach((subscriber) -> {
-            if (!subscriber.isFull()) {
+        LinkedList<Subscriber> eventSubscribers = subscribers.get(message.getClass());
 
-                subscriber.type = message.getClass();
-
-                subscriber.addMessage(message);
-                if (!subscriber.isHandlingMessage)
-                    subscriber.condition.signal();
-            }
-        });
+        if (eventSubscribers != null) {
+            eventSubscribers.forEach((subscriber) -> {
+                if (!subscriber.isFull()) {
+                    subscriber.addMessage(message);
+                    if (!subscriber.isHandlingMessages)
+                        subscriber.condition.signal();
+                }
+            });
+        }
 
         monitor.unlock();
     }
 
-
-    public void Shutdown() {
+    public void Shutdown() throws InterruptedException {
         monitor.lock();
-        shutdown = true;
-        monitor.unlock();
+
+        // set the flag to stop new subscribers and publishers to register handlers or publish new messages
+        isShuttingDown = true;
+
+        //  awake all subscribers that are sleeping so they begin to handle their messages
+        //  shutdown will only be possible when all of them are done
+        shutdown.signalAll();
+
+        try {
+            while (true) {
+                //check first to avoid infinite loop in case shutdown is called with no subscribers
+                if(numberOfSubscribers == 0){
+                    monitor.unlock();
+                    return;
+                }
+
+                // wait indefinitely for the subscribers to be done
+                shutdown.await(Integer.MAX_VALUE, TimeUnit.MILLISECONDS);
+            }
+        } catch (InterruptedException e) {
+            isShuttingDown = false;
+            throw e;
+        }finally {
+            monitor.unlock();
+        }
+    }
+
+    private <T> void handleMessages (LinkedList<Object> messages, Consumer<T> handler) throws InterruptedException {
+        while (messages.size() > 0) {
+            T message = (T) messages.removeFirst();
+            try {
+                handler.accept(message);
+            } catch (Exception e) {
+                String errorMessage = String.format("Handler error : %s", e.getMessage());
+                throw new InterruptedException(errorMessage);
+            }
+        }
     }
 
     private class Subscriber {
         private final Condition condition;
         private LinkedList<Object> messages;
-        private boolean isHandlingMessage;
-        public Class type;
+        private boolean isHandlingMessages;
 
-        Subscriber(Condition condition, Class type) {
+        Subscriber(Condition condition) {
             this.condition = condition;
             messages = new LinkedList<>();
-            isHandlingMessage = false;
-            this.type = type;
+            isHandlingMessages = false;
         }
 
         void addMessage(Object message) {
-            messages.add(message);
+            messages.addLast(message);
         }
 
         boolean isFull() {
             return messages.size() >= MAX_PENDING;
         }
+
     }
 }
