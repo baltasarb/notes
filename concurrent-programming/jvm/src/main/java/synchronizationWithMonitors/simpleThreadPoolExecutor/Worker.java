7e1f944ddb19c0dea84bf932ab6a962891396849
baltasarb <baltasar.brito@gmail.com>
2018-10-22 19:42:00 +0100

    Update: cleaned worker class.

--- concurrent-programming/jvm/src/main/java/synchronizationWithMonitors/simpleThreadPoolExecutor/Worker.java
+++ concurrent-programming/jvm/src/main/java/synchronizationWithMonitors/simpleThreadPoolExecutor/Worker.java
@@ -56,34 +56,7 @@ class Worker {
                 }
 
                 if (work != null) {
-                    poolMonitor.unlock();
-
-                    try {
-                        //work done outside the exclusion to allow for parallelism, prevent possible deadlocks
-                        work.run();
-                    } catch (Exception e) {
-                        //exception ignored
-                        //the pool should not be affected by any exception occurring outside of it
-                    } finally {
-                        poolMonitor.lock();
-                    }
-
-                    work = null;
-
-                    //renew waiting time so it doesnt expire after work is done
-                    timer.reset(keepAliveTime);
-
-                    //when thread is not idle it will not be notified by the shutdown from the pool
-                    //so a check is needed here to know if this worker should stop working
-                    //in this case the worker wont go to idle list and will get the work
-                    //from the executor himself
-                    if (poolFunctions.poolIsShuttingDown()) {
-                        workerIsShuttingDown = true;
-                    }
-                    //submitWork -> shutdown (before executeWork continues)
-                    else {
-                        poolFunctions.addIdleWorker(this);
-                    }
+                    doWork(timer, keepAliveTime);
                 }
 
                 boolean executorsAreWaiting = poolFunctions.executorsAreWaiting();
@@ -97,13 +70,7 @@ class Worker {
                 }
 
                 if (workerIsShuttingDown && !executorsAreWaiting) {
-                    //if present remove from idle list
-                    poolFunctions.removeIdleWorker(this);
-
-                    poolFunctions.decrementWorkerCount();
-                    if (poolFunctions.poolIsReadyToShutdown()) {
-                        poolFunctions.notifyAwaitTermination();
-                    }
+                    terminate();
                     return;
                 }
 
@@ -131,4 +98,47 @@ class Worker {
         workerIsShuttingDown = true;
     }
 
+    private void doWork(Timer timer, int keepAliveTime) {
+        poolMonitor.unlock();
+
+        try {
+            //work done outside the exclusion to allow for parallelism, prevent possible deadlocks
+            work.run();
+        } catch (Exception e) {
+            //exception ignored
+            //the pool should not be affected by any exception occurring outside of it
+        } finally {
+            poolMonitor.lock();
+        }
+
+        work = null;
+
+        //renew waiting time so it doesnt expire after work is done
+        timer.reset(keepAliveTime);
+
+        //when thread is not idle it will not be notified by the shutdown from the pool
+        //so a check is needed here to know if this worker should stop working
+        //in this case the worker wont go to idle list and will get the work
+        //from the executor himself
+        if (poolFunctions.poolIsShuttingDown()) {
+            workerIsShuttingDown = true;
+        }
+        //submitWork -> shutdown (before executeWork continues)
+        else {
+            poolFunctions.addIdleWorker(this);
+        }
+    }
+
+    private void terminate() {
+        //if present remove from idle list
+        poolFunctions.removeIdleWorker(this);
+
+        poolFunctions.decrementWorkerCount();
+        if (poolFunctions.poolIsReadyToShutdown()) {
+            poolFunctions.notifyAwaitTermination();
+        }
+    }
+
 }
+
+
