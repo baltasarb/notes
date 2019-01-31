ac36de7a0d6e96701516bf0cb6b7e8b88eb860c0
baltasarb <baltasar.brito@gmail.com>
2019-01-31 17:31:06 +0000

    removed unecessary android projects

--- android/kotlin/Yama/app/src/main/java/baltasarb/yama/model/Team.kt
+++
@@ -1,7 +0,0 @@
-package baltasarb.yama.model
-
-data class Team(
-    val id: Int,
-    val name: String,
-    val description : String?
-)
--- android/kotlin/Yama/app/src/main/java/baltasarb/yama/model/TeamMember.kt
+++
@@ -1,6 +0,0 @@
-package baltasarb.yama.model
-
-data class TeamMember(
-    val login: String,
-    val avatar_url: String
-)
