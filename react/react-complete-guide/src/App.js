909644ee236f8b4f2af4c91f7566496e11d130c0
Baltasar Brito <baltasar.brito@farfetch.com>
2019-11-12 18:07:59 +0000

    feat: Intro

--- react/react-complete-guide/src/App.js
+++ react/react-complete-guide/src/App.js
@@ -1,26 +1,28 @@
+//the reason react is imported is because iteration 1, behind the scenes, is equal to iteration 3.
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //Iteration 1
+  // return (
+  //   <div className="App">
+  //     <h1>React Application</h1>
+  //   </div>
+  // );
+  
+  //Iteration 2
+  //creates div with two lines of text inside
+  //cecause h1 and React Application are interpreted as text
+  //return React.createElement('div', null, 'h1', 'React Application'); 
+
+  //Iteration 2
+  //creates a div with an h1 tag inside, which haves React Application text inside
+  //the css is not considered
+  //return React.createElement('div', null, React.createElement('h1', null,  'React Application'));
+
+  //Iteration 3
+  //this code is the exact same code as iteration 1
+  return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'React Application'));
 }
 
 export default App;
